0b7e41c71a44ab67db4c28854394d24b93f24d92
Ryan Thompson <service@aiwebsystems.com>
2015-07-09 02:14:35 -0500

    Ya

---
+++ src/BlocksModule.php
@@ -0,0 +1,33 @@
+<?php namespace Anomaly\BlocksModule;
+
+use Anomaly\Streams\Platform\Addon\Module\Module;
+
+/**
+ * Class BlocksModule
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\BlocksModule
+ */
+class BlocksModule extends Module
+{
+
+    /**
+     * The module addon.
+     *
+     * @var string
+     */
+    protected $icon = 'cubes';
+
+    /**
+     * The module sections.
+     *
+     * @var array
+     */
+    protected $sections = [
+        'blocks',
+        'groups'
+    ];
+
+}
