5498139bc6968387c7472ffc76c3093a0b5fd590
Ryan Thompson <service@aiwebsystems.com>
2015-07-09 03:37:58 -0500

    Lang

---
+++ resources/lang/en/field.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'name'        => [
+        'name' => 'Name'
+    ],
+    'slug'        => [
+        'name' => 'Slug'
+    ],
+    'description' => [
+        'name' => 'Description'
+    ],
+    'group'       => [
+        'name'         => 'Group',
+        'instructions' => 'Choose an optional group to add this block to.',
+        'placeholder'  => 'Choose a group'
+    ]
+];
