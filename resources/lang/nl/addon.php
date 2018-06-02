1737d929e8041dfa87b03975c8fd0bb9f134f106
Dwain Scheeren <info@dwain.nl>
2018-06-02 19:25:37 +0200

    Create addon.php

---
+++ resources/lang/nl/addon.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'title'       => 'Blocks',
+    'name'        => 'Blocks Module',
+    'description' => 'Manage small sections of self-contained blocks of logic.',
+    'section'     => [
+        'groups' => 'Groups',
+    ],
+];
